dcbc44d84dac55a12ef52ad94d014df1f917a9be
kerowagdy <kerowagdy2018@gmail.com>
2026-06-03 19:02:03 +0300

    Refactored AudioPlayer.cs (code resturcture only)

--- core/AudioPlayer.cs
+++ core/AudioPlayer.cs
@@ -4,69 +4,81 @@ namespace Trackey;
 
 class AudioPlayer
 {
-    private LibVLC libvlc;
-    private MediaPlayer player;
-    private Media? currentMedia;
+    /* Fields */
+    private readonly LibVLC _libvlc;
+    private readonly MediaPlayer _player;
+    private Media? _currentMedia;
 
-    public event EventHandler? TrackEnded;
-
+    /* Ctors */
     public AudioPlayer()
     {
+        // if (OperatingSystem.IsWindows())
+        //     Core.Initialize(@"C:\Program Files\VideoLAN\VLC");
+        // else
         Core.Initialize();
-        libvlc = new LibVLC();
-        player = new MediaPlayer(libvlc);
-        player.EndReached += (_, _) =>
+
+        _libvlc = new LibVLC();
+        _player = new MediaPlayer(_libvlc);
+        _player.EndReached += (_, _) =>
         {
             TrackEnded?.Invoke(this, EventArgs.Empty);
         };
     }
 
+    /* Events */
+    public event EventHandler? TrackEnded;
+
+    /* Properties */
+    public int Volume => IsMuted ? 0 : _player.Volume;
+    public PlayerState State =>
+        _player.State switch
+        {
+            VLCState.Playing => PlayerState.PLAYING,
+            VLCState.Paused => PlayerState.PAUSED,
+            _ => PlayerState.NONE,
+        };
+    public bool IsPlaying => State == PlayerState.PLAYING;
+    public long TimeMs => _player.Time;
+    public long DurationMs => _player.Length;
+    public bool IsMuted => _player.Mute;
+
+
+    /* Methods */
+    public void Init()
+    {
+
+    }
+    public void Play(string filename)
+    {
+        _currentMedia?.Dispose();
+
+        _currentMedia = new Media(_libvlc, filename, FromType.FromPath);
+        _player.Media = _currentMedia;
+
+        Logger.Log($"Play {filename} ({File.Exists(filename)})");
+        _player.Play();
+    }
+    public void Stop() => _player.Stop();
+
+    public void Pause() => _player.Pause();
+    public void Resume() => _player.SetPause(false);
+
+    public void TogglePause()
+    {
+        if (_player.IsPlaying) Pause();
+        else Resume();
+    }
+    public void ToggleMute() => _player.ToggleMute();
+
+    public void SetVolume(int volume) => _player.Volume = Math.Max(Math.Min(volume, 100), 0);
+    public void IncreaseVolume(int incBy) => SetVolume(Volume + incBy);
+    public void DecreaseVolume(int decBy) => SetVolume(Volume - decBy);
+
+    /* Nested types */
     public enum PlayerState
     {
         NONE,
         PLAYING,
         PAUSED,
     }
-
-    public int Volume => IsMuted ? 0 : player.Volume;
-    public PlayerState State =>
-        player.State switch
-        {
-            VLCState.Playing => PlayerState.PLAYING,
-            VLCState.Paused => PlayerState.PAUSED,
-            _ => PlayerState.NONE,
-        };
-
-    public bool IsPlaying => State == PlayerState.PLAYING;
-
-
-    public void Play(string filename)
-    {
-        currentMedia?.Dispose();
-
-        currentMedia = new Media(libvlc, filename, FromType.FromPath);
-        player.Media = currentMedia;
-
-        Logger.Log($"Play {filename} ({File.Exists(filename)})");
-        player.Play();
-    }
-
-    public long TimeMs => player.Time;
-    public long DurationMs => player.Length;
-
-    public void Stop() => player.Stop();
-    public void Pause() => player.Pause();
-    public void Resume() => player.SetPause(false);
-    public void TogglePause()
-    {
-        if (player.IsPlaying) Pause();
-        else Resume();
-    }
-
-    public void ToggleMute() => player.ToggleMute();
-    public bool IsMuted => player.Mute;
-
-    public void SetVolume(int volume) => player.Volume = Math.Max(Math.Min(volume, 100), 0);
-    public void IncreaseVolume(int incBy) => SetVolume(Volume + incBy);
-    public void DecreaseVolume(int decBy) => SetVolume(Volume - decBy);
 }
