5cfac17cc8e47a4c73069936a881f1b018114877
kerowagdy <kerowagdy2018@gmail.com>
2026-06-03 19:01:32 +0300

    Refactored Application.cs (code resturcture only)

--- core/Application.cs
+++ core/Application.cs
@@ -7,9 +7,25 @@ namespace Trackey;
 
 class Application
 {
+    /* Constants */
     public const int TARGET_FPS = 30;
 
-    /* Services */
+    /* Fields */
+    private bool nextTrackRequested = false;
+    private List<DownloadTaskInfo> ActiveDownloads = [];
+    private Dictionary<Guid, Notification> ActiveNotifications = [];
+    private Stack<Screen> BackScreens = new();
+    private Stack<Screen> ForwardScreens = new();
+
+    /* Ctors */
+    public Application()
+    {
+        NavigateTo(new HomeScreen(this), false);
+        Player.TrackEnded += OnTrackEnded;
+    }
+
+    /* Properties */
+    // Services
     public Library Lib { get; } = new();
     public AudioPlayer Player { get; } = new();
     public QueueManager Queue { get; } = new();
@@ -20,242 +36,26 @@ class Application
     /* Application State */
     public bool IsRunning { get; set; } = true;
 
-    // Current User
     public Guid? CurrUserId { get; set; }
     public User? CurrUser =>
         CurrUserId is Guid id && Users.TryGetUser(id, out User? user) ? user
         : null;
     public bool LoggedIn => CurrUserId is not null;
 
-    // Current Track
     public Guid? CurrTrackId { get; set; }
     public Track? CurrTrack =>
         CurrTrackId is Guid id && Lib.TryGetTrack(id, out var track) ? track
         : null;
     public bool TrackExists => CurrTrackId is not null;
-    private bool nextTrackRequested = false;
 
-    public List<DownloadTaskInfo> ActiveDownloads = [];
-    public Dictionary<Guid, Notification> ActiveNotifications = [];
 
     // Ui
-    private Ui ui = new();
+    private UI Ui { get; } = new();
     public Screen CurrentScreen { get; set; } = null!;
     public Prompt? ActivePrompt { get; set; } = null;
-    public Stack<Screen> backScreens = new();
-    public Stack<Screen> forwardScreens = new();
     public bool PlaybackControlsUnlocked { get; set; } = false;
 
-    public async Task InitializeAsync()
-    {
-        Paths.Init();
-        Logger.Clear();
-
-        var usersLoaded = await Users.LoadUsers();
-        if (!usersLoaded.Success)
-            AddNotification(Notification.Error(usersLoaded.ErrorMessage ?? "Failed to load users"));
-        else
-            AddNotification(Notification.Success("Loaded users successfully"));
-
-        var libLoaded = await Lib.LoadLibrary();
-        if (!libLoaded.Success)
-            AddNotification(Notification.Error(usersLoaded.ErrorMessage ?? "Failed to load users"));
-        else
-            AddNotification(Notification.Success("Loaded library successfully"));
-    }
-
-    public async Task FinalizeAsync()
-    {
-        var usersSaved = await Users.SaveUsers();
-        if (!usersSaved.Success)
-            AddNotification(Notification.Error(usersSaved.ErrorMessage ?? "Failed to save users"));
-        else
-            AddNotification(Notification.Success("Saved users successfully"));
-        var libSaved = await Lib.SaveLibrary();
-        if (!libSaved.Success)
-            AddNotification(Notification.Error(libSaved.ErrorMessage ?? "Failed to save library"));
-        else
-            AddNotification(Notification.Success("Saved library successfully"));
-    }
-
-    public Application()
-    {
-        NavigateTo(new HomeScreen(this), false);
-        Player.TrackEnded += OnTrackEnded;
-    }
-
-    public void Run()
-    {
-        AnsiConsole.Live(ui.Layout)
-        .Start(ctx =>
-        {
-
-            while (IsRunning)
-            {
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey(true);
-                    HandleKey(key);
-                }
-
-                Update();
-
-                ctx.Refresh();
-                Thread.Sleep(1000 / TARGET_FPS);
-            }
-
-        });
-    }
-
-    // --------------------------------------------------
-
-    public void Update()
-    {
-        if (!TrackExists && !Queue.IsEmpty)
-            OnTrackEnded(null, new());
-        UpdateQueue();
-
-        UpdateActiveDownloads();
-        UpdateNotifications();
-
-        ui.Update(CurrentScreen,
-            new PlaybackInfo(
-                Player.State,
-                Player.Volume,
-                CurrTrack,
-                CurrUser?.Username,
-                PlaybackControlsUnlocked,
-                Player.TimeMs,
-                Player.DurationMs
-                ),
-            GetQueueItems(),
-            ActiveDownloads,
-            ActivePrompt,
-            ActiveNotifications.Values
-        );
-    }
-
-    public void UpdateQueue()
-    {
-        if (!nextTrackRequested || Queue.IsEmpty)
-            return;
-
-        nextTrackRequested = false;
-        SetCurrentTrack(Queue.Next());
-
-        Logger.Log("Next Track Invoked");
-    }
-
-    public void PlayTrackNow(Guid trackId)
-    {
-        if (CurrTrackId == trackId)
-            SetCurrentTrack(trackId);
-        else
-        {
-            Queue.PlayNext(trackId);
-            SetCurrentTrack(Queue.Next());
-        }
-    }
-
-
-    public void SetActivePrompt(Prompt prompt) => ActivePrompt = prompt;
-    public void RemoveActivePrompt() => ActivePrompt = null;
-
-    // Removes Active Downloads Completed before 4 seconds or more
-    public void UpdateActiveDownloads()
-    {
-        var now = DateTime.Now;
-        ActiveDownloads.RemoveAll(
-        t => t.CompletedAt is DateTime completed
-        && (now - completed).TotalSeconds >= 4
-        );
-    }
-
-    // Removes Notifications Completed before 4 seconds or more
-    public void UpdateNotifications()
-    {
-        foreach (var n in ActiveNotifications.Where(n => n.Value.IsExpired()))
-            ActiveNotifications.Remove(n.Key);
-    }
-
-    public IEnumerable<QueueItem> GetQueueItems()
-    {
-        var items = Queue.QueueItems();
-        foreach (var item in items)
-        {
-            if (Lib.TryGetTrack(item.Track.Id, out Track? track))
-                yield return new QueueItem(track, item.Type);
-        }
-    }
-
-
-    // Screen Navigators
-    public void NavigateTo(Screen nextScreen, bool saveHistory)
-    {
-        if (saveHistory && CurrentScreen != null)
-            backScreens.Push(CurrentScreen);
-
-        Logger.Log($"{CurrentScreen?.GetType()} => {nextScreen.GetType()}, {(saveHistory ? "saving" : "not saving")}");
-
-        CurrentScreen = nextScreen;
-
-        ClearForwardScreens();
-    }
-
-    public void NavigateBack(bool saveHistory = true)
-    {
-        if (backScreens.Count == 0)
-            return;
-        if (saveHistory)
-            forwardScreens.Push(CurrentScreen);
-        CurrentScreen = backScreens.Pop();
-    }
-
-    public void NavigateForward(bool saveHistory = true)
-    {
-        if (forwardScreens.Count == 0)
-            return;
-        if (saveHistory)
-            backScreens.Push(CurrentScreen);
-        CurrentScreen = forwardScreens.Pop();
-    }
-
-    public void ClearBackScreens() => backScreens.Clear();
-    public void ClearForwardScreens() => forwardScreens.Clear();
-
-
-    // Key Handlers
-    public void HandleKey(ConsoleKeyInfo key)
-    {
-        // Logger.Log($"{key.KeyChar} pressed [{key.Key}]");
-        if (key.KeyChar == ';')
-        {
-            TogglePlaybackControls();
-            return;
-        }
-
-        if (HandleGlobalShortcuts(key))
-            return;
-
-        if (PlaybackControlsUnlocked)
-            HandlePlaybackShortcuts(key);
-        else if (ActivePrompt is not null)
-            ActivePrompt.HandleInput(key);
-        else
-            CurrentScreen.HandleInput(key);
-    }
-
-    public void TogglePlaybackControls() => PlaybackControlsUnlocked ^= true;
-
-    public void HandlePlaybackShortcuts(ConsoleKeyInfo key)
-    {
-        if (key.KeyChar == '+') Player.IncreaseVolume(5);
-        else if (key.KeyChar == '-') Player.DecreaseVolume(5);
-        else if (key.Key == ConsoleKey.Spacebar) Player.TogglePause();
-        else if (key.Key == ConsoleKey.M) Player.ToggleMute();
-    }
-
-    public IEnumerable<Shortcut> GlobalShortcuts => [
+    public static IEnumerable<Shortcut> GlobalShortcuts => [
         new Shortcut() {
             Description = "[Q]uit Application",
             Combo = new KeyCombo(ConsoleKey.Q, Ctrl: true)
@@ -278,6 +78,146 @@ class Application
         },
     ];
 
+    
+    // --------------------------------------------------
+    /* Methods */
+    // --------------------------------------------------
+
+    public async Task InitializeAsync()
+    {
+        Paths.Init();
+        Logger.Clear();
+
+        var usersLoaded = await Users.LoadUsers();
+        if (!usersLoaded.Success)
+            AddNotification(Notification.Error(usersLoaded.ErrorMessage ?? "Failed to load users"));
+        else
+            AddNotification(Notification.Success("Loaded users successfully"));
+
+        var libLoaded = await Lib.LoadLibrary();
+        if (!libLoaded.Success)
+            AddNotification(Notification.Error(usersLoaded.ErrorMessage ?? "Failed to load users"));
+        else
+            AddNotification(Notification.Success("Loaded library successfully"));
+    }
+    public async Task FinalizeAsync()
+    {
+        var usersSaved = await Users.SaveUsers();
+        if (!usersSaved.Success)
+            AddNotification(Notification.Error(usersSaved.ErrorMessage ?? "Failed to save users"));
+        else
+            AddNotification(Notification.Success("Saved users successfully"));
+        var libSaved = await Lib.SaveLibrary();
+        if (!libSaved.Success)
+            AddNotification(Notification.Error(libSaved.ErrorMessage ?? "Failed to save library"));
+        else
+            AddNotification(Notification.Success("Saved library successfully"));
+    }
+
+
+    public void Run()
+    {
+        AnsiConsole.Live(Ui.Layout)
+        .Start(ctx =>
+        {
+
+            while (IsRunning)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    HandleKey(key);
+                }
+
+                Update();
+
+                ctx.Refresh();
+                Thread.Sleep(1000 / TARGET_FPS);
+            }
+
+        });
+    }
+
+
+    public void Update()
+    {
+        if (!TrackExists && !Queue.IsEmpty)
+            OnTrackEnded(null, new());
+        UpdateQueue();
+
+        UpdateActiveDownloads();
+        UpdateNotifications();
+
+        Ui.Update(CurrentScreen,
+            new PlaybackInfo(
+                Player.State,
+                Player.Volume,
+                CurrTrack,
+                CurrUser?.Username,
+                PlaybackControlsUnlocked,
+                Player.TimeMs,
+                Player.DurationMs
+                ),
+            GetQueueItems(),
+            ActiveDownloads,
+            ActivePrompt,
+            ActiveNotifications.Values
+        );
+    }
+    public void UpdateQueue()
+    {
+        if (!nextTrackRequested || Queue.IsEmpty)
+            return;
+
+        nextTrackRequested = false;
+        SetCurrentTrack(Queue.Next());
+
+        Logger.Log("Next Track Invoked");
+    }
+    public void UpdateActiveDownloads()
+    {
+        // Removes Active Downloads Completed before 4 seconds or more
+        var now = DateTime.Now;
+        ActiveDownloads.RemoveAll(
+        t => t.CompletedAt is DateTime completed
+        && (now - completed).TotalSeconds >= 4
+        );
+    }
+    public void UpdateNotifications()
+    {
+        // Removes Notifications Completed before 4 seconds or more
+        foreach (var n in ActiveNotifications.Where(n => n.Value.IsExpired()))
+            ActiveNotifications.Remove(n.Key);
+    }
+
+
+    // Key Handlers
+    public void HandleKey(ConsoleKeyInfo key)
+    {
+        // Logger.Log($"{key.KeyChar} pressed [{key.Key}]");
+        if (key.KeyChar == ';')
+        {
+            TogglePlaybackControls();
+            return;
+        }
+
+        if (HandleGlobalShortcuts(key))
+            return;
+
+        if (PlaybackControlsUnlocked)
+            HandlePlaybackShortcuts(key);
+        else if (ActivePrompt is not null)
+            ActivePrompt.HandleInput(key);
+        else
+            CurrentScreen.HandleInput(key);
+    }
+    public void HandlePlaybackShortcuts(ConsoleKeyInfo key)
+    {
+        if (key.KeyChar == '+') Player.IncreaseVolume(5);
+        else if (key.KeyChar == '-') Player.DecreaseVolume(5);
+        else if (key.Key == ConsoleKey.Spacebar) Player.TogglePause();
+        else if (key.Key == ConsoleKey.M) Player.ToggleMute();
+    }
     public bool HandleGlobalShortcuts(ConsoleKeyInfo key)
     {
         bool ctrl = key.Modifiers.HasFlag(ConsoleModifiers.Control);
@@ -306,19 +246,52 @@ class Application
         return true;
     }
 
-    // Mutating State
-    public void SetCurrentUser(Guid? userId)
+
+    // Screen Navigators
+    public void NavigateTo(Screen nextScreen, bool saveHistory)
     {
-        CurrUserId = userId;
+        if (saveHistory && CurrentScreen != null)
+            BackScreens.Push(CurrentScreen);
+
+        Logger.Log($"{CurrentScreen?.GetType()} => {nextScreen.GetType()}, {(saveHistory ? "saving" : "not saving")}");
+
+        CurrentScreen = nextScreen;
+
+        ClearForwardScreens();
+    }
+    public void NavigateBack(bool saveHistory = true)
+    {
+        if (BackScreens.Count == 0)
+            return;
+        if (saveHistory)
+            ForwardScreens.Push(CurrentScreen);
+        CurrentScreen = BackScreens.Pop();
+    }
+    public void NavigateForward(bool saveHistory = true)
+    {
+        if (ForwardScreens.Count == 0)
+            return;
+        if (saveHistory)
+            BackScreens.Push(CurrentScreen);
+        CurrentScreen = ForwardScreens.Pop();
+    }
+    public void ClearBackScreens() => BackScreens.Clear();
+    public void ClearForwardScreens() => ForwardScreens.Clear();
+
+
+    // Queue Service
+    // Returns the corresponding Track for each trackId from Queue.QueueItems
+    public IEnumerable<QueueItem> GetQueueItems()
+    {
+        var items = Queue.QueueItems();
+        foreach (var item in items)
+        {
+            if (Lib.TryGetTrack(item.Track.Id, out Track? track))
+                yield return new QueueItem(track, item.Type);
+        }
     }
 
-    private void SetCurrentTrack(Guid trackId)
-    {
-        CurrTrackId = trackId;
-        if (CurrTrack is not null)
-            Player.Play(CurrTrack.Filepath);
-    }
-
+    // Download Service
     public async Task AddDownload(string url, string title, string artist)
     {
         var taskInfo = new DownloadTaskInfo()
@@ -358,18 +331,47 @@ class Application
         Queue.Enqueue(track.Id);
     }
 
+
+    // Active Prompt
+    public void SetActivePrompt(Prompt prompt) => ActivePrompt = prompt;
+    public void RemoveActivePrompt() => ActivePrompt = null;
+
+    // Mutating State
+    public void SetCurrentUser(Guid? userId)
+    {
+        CurrUserId = userId;
+    }
+    private void SetCurrentTrack(Guid trackId)
+    {
+        CurrTrackId = trackId;
+        if (CurrTrack is not null)
+            Player.Play(CurrTrack.Filepath);
+    }
+    public void TogglePlaybackControls() => PlaybackControlsUnlocked ^= true;
+    public void PlayTrackNow(Guid trackId)
+    {
+        if (CurrTrackId == trackId)
+            SetCurrentTrack(trackId);
+        else
+        {
+            Queue.PlayNext(trackId);
+            SetCurrentTrack(Queue.Next());
+        }
+    }
+
+    // Notifications
     public Guid AddNotification(Notification notification)
     {
         Guid guid = Guid.NewGuid();
         ActiveNotifications[guid] = notification;
         return guid;
     }
-
     public void RemoveNotification(Guid notificationId)
     {
         ActiveNotifications.Remove(notificationId);
     }
 
+    // Library
     public async void CreatePlaylist(string playlistTitle)
     {
         var playlist = new Playlist() {
